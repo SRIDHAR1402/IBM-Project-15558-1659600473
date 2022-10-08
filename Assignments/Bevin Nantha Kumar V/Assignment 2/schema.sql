44e55785a7435ecda9123988334ed608a45455bf
Bevinnantha <112535332+Bevinnantha@users.noreply.github.com>
2022-10-08 09:10:02 -0700

    Add files via upload

---
+++ Assignments/Bevin Nantha Kumar V/Assignment 2/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+INSERT INTO users (username, password, name, email) VALUES (
+    "AGS",
+    "abcde",
+    "Andrew",
+    "ags123@gmail.com"
+);
